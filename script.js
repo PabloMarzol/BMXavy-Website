4b1ef1e95688591c0073f9df35e20a2e5b6631a1
Pablo Marzol <pablomarzol@yandex.com>
2022-09-19 19:37:02 +0800

    nav-bar fixed

---
+++ script.js
@@ -0,0 +1,697 @@
+/*    MAIN
+BUTTON FUNCTION*/
+
+
+
+const make_imageBig1 = document.querySelector(".photo_1");
+const make_imageBig2 = document.querySelector(".photo_2");
+const make_imageBig3 = document.querySelector(".photo_3");
+const make_imageBig4 = document.querySelector(".photo_4");
+const make_imageBig5 = document.querySelector(".photo_5");
+const make_imageBig6 = document.querySelector(".photo_6");
+const make_imageBig7 = document.querySelector(".photo_7");
+const make_imageBig8 = document.querySelector(".photo_8");
+const make_imageBig9 = document.querySelector(".photo_9");
+const make_imageBig10 = document.querySelector(".photo_10");
+const make_imageBig11 = document.querySelector(".photo_11");
+const make_imageBig12 = document.querySelector(".photo_12");
+const make_imageBig13 = document.querySelector(".photo_13");
+const make_imageBig14 = document.querySelector(".photo_14");
+const make_imageBig15 = document.querySelector(".photo_15");
+const make_imageBig16 = document.querySelector(".photo_16");
+const make_imageBig17 = document.querySelector(".photo_17");
+const make_imageBig18 = document.querySelector(".photo_18");
+const make_imageBig19 = document.querySelector(".photo_19");
+const make_imageBig20 = document.querySelector(".photo_20");
+const make_imageBig21 = document.querySelector(".photo_21");
+const make_imageBig22= document.querySelector(".photo_22");
+
+
+function makeBig1() {
+   
+   const bigPhoto1 = make_imageBig1.getAttribute("data-visible");
+
+   if (bigPhoto1 === "false") {
+
+      make_imageBig1.setAttribute("data-visible", true);
+
+   } else if (bigPhoto1 === "true") {
+
+      make_imageBig1.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig2() {
+   
+   const bigPhoto2 = make_imageBig2.getAttribute("data-visible");
+
+   if (bigPhoto2 === "false") {
+
+      make_imageBig2.setAttribute("data-visible", true);
+
+   } else if (bigPhoto2 === "true") {
+
+      make_imageBig2.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig3() {
+   
+   const bigPhoto3 = make_imageBig3.getAttribute("data-visible");
+
+   if (bigPhoto3 === "false") {
+
+      make_imageBig3.setAttribute("data-visible", true);
+
+   } else if (bigPhoto3 === "true") {
+
+      make_imageBig3.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig4() {
+   
+   const bigPhoto4 = make_imageBig4.getAttribute("data-visible");
+
+   if (bigPhoto4 === "false") {
+
+      make_imageBig4.setAttribute("data-visible", true);
+
+   } else if (bigPhoto4 === "true") {
+
+      make_imageBig4.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig5() {
+   
+   const bigPhoto5 = make_imageBig5.getAttribute("data-visible");
+
+   if (bigPhoto5 === "false") {
+
+      make_imageBig5.setAttribute("data-visible", true);
+
+   } else if (bigPhoto5 === "true") {
+
+      make_imageBig5.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig6() {
+   
+   const bigPhoto6 = make_imageBig6.getAttribute("data-visible");
+
+   if (bigPhoto6 === "false") {
+
+      make_imageBig6.setAttribute("data-visible", true);
+
+   } else if (bigPhoto6 === "true") {
+
+      make_imageBig6.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig7() {
+   
+   const bigPhoto7 = make_imageBig7.getAttribute("data-visible");
+
+   if (bigPhoto7 === "false") {
+
+      make_imageBig7.setAttribute("data-visible", true);
+
+   } else if (bigPhoto7 === "true") {
+
+      make_imageBig7.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig8() {
+   
+   const bigPhoto8 = make_imageBig8.getAttribute("data-visible");
+
+   if (bigPhoto8 === "false") {
+
+      make_imageBig8.setAttribute("data-visible", true);
+
+   } else if (bigPhoto8 === "true") {
+
+      make_imageBig8.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig9() {
+   
+   const bigPhoto9 = make_imageBig9.getAttribute("data-visible");
+
+   if (bigPhoto9 === "false") {
+
+      make_imageBig9.setAttribute("data-visible", true);
+
+   } else if (bigPhoto9 === "true") {
+
+      make_imageBig9.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig10() {
+   
+   const bigPhoto10 = make_imageBig10.getAttribute("data-visible");
+
+   if (bigPhoto10 === "false") {
+
+      make_imageBig10.setAttribute("data-visible", true);
+
+   } else if (bigPhoto10 === "true") {
+
+      make_imageBig10.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig11() {
+   
+   const bigPhoto11 = make_imageBig11.getAttribute("data-visible");
+
+   if (bigPhoto11 === "false") {
+
+      make_imageBig11.setAttribute("data-visible", true);
+
+   } else if (bigPhoto11 === "true") {
+
+      make_imageBig11.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig12() {
+   
+   const bigPhoto12 = make_imageBig12.getAttribute("data-visible");
+
+   if (bigPhoto12 === "false") {
+
+      make_imageBig12.setAttribute("data-visible", true);
+
+   } else if (bigPhoto12 === "true") {
+
+      make_imageBig12.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig13() {
+   
+   const bigPhoto13 = make_imageBig13.getAttribute("data-visible");
+
+   if (bigPhoto13 === "false") {
+
+      make_imageBig13.setAttribute("data-visible", true);
+
+   } else if (bigPhoto13 === "true") {
+
+      make_imageBig13.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig14() {
+   
+   const bigPhoto14 = make_imageBig14.getAttribute("data-visible");
+
+   if (bigPhoto14 === "false") {
+
+      make_imageBig14.setAttribute("data-visible", true);
+
+   } else if (bigPhoto14 === "true") {
+
+      make_imageBig14.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig15() {
+   
+   const bigPhoto15 = make_imageBig15.getAttribute("data-visible");
+
+   if (bigPhoto15 === "false") {
+
+      make_imageBig15.setAttribute("data-visible", true);
+
+   } else if (bigPhoto15 === "true") {
+
+      make_imageBig15.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig16() {
+   
+   const bigPhoto16 = make_imageBig16.getAttribute("data-visible");
+
+   if (bigPhoto16 === "false") {
+
+      make_imageBig16.setAttribute("data-visible", true);
+
+   } else if (bigPhoto16 === "true") {
+
+      make_imageBig16.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig17() {
+   
+   const bigPhoto17 = make_imageBig17.getAttribute("data-visible");
+
+   if (bigPhoto17 === "false") {
+
+      make_imageBig17.setAttribute("data-visible", true);
+
+   } else if (bigPhoto17 === "true") {
+
+      make_imageBig17.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig18() {
+   
+   const bigPhoto18 = make_imageBig18.getAttribute("data-visible");
+
+   if (bigPhoto18 === "false") {
+
+      make_imageBig18.setAttribute("data-visible", true);
+
+   } else if (bigPhoto18 === "true") {
+
+      make_imageBig18.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig19() {
+   
+   const bigPhoto19 = make_imageBig19.getAttribute("data-visible");
+
+   if (bigPhoto19 === "false") {
+
+      make_imageBig19.setAttribute("data-visible", true);
+
+   } else if (bigPhoto19 === "true") {
+
+      make_imageBig19.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig20() {
+   
+   const bigPhoto20 = make_imageBig20.getAttribute("data-visible");
+
+   if (bigPhoto20 === "false") {
+
+      make_imageBig20.setAttribute("data-visible", true);
+
+   } else if (bigPhoto20 === "true") {
+
+      make_imageBig20.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig21() {
+   
+   const bigPhoto21 = make_imageBig21.getAttribute("data-visible");
+
+   if (bigPhoto21 === "false") {
+
+      make_imageBig21.setAttribute("data-visible", true);
+
+   } else if (bigPhoto21 === "true") {
+
+      make_imageBig21.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig22() {
+   
+   const bigPhoto22 = make_imageBig22.getAttribute("data-visible");
+
+   if (bigPhoto22 === "false") {
+
+      make_imageBig22.setAttribute("data-visible", true);
+
+   } else if (bigPhoto22 === "true") {
+
+      make_imageBig22.setAttribute("data-visible", false);
+   }
+
+}
+
+make_imageBig1.addEventListener("click", makeBig1);
+make_imageBig2.addEventListener("click", makeBig2);
+make_imageBig3.addEventListener("click", makeBig3);
+make_imageBig4.addEventListener("click", makeBig4);
+make_imageBig5.addEventListener("click", makeBig5);
+make_imageBig6.addEventListener("click", makeBig6);
+make_imageBig7.addEventListener("click", makeBig7);
+make_imageBig8.addEventListener("click", makeBig8);
+make_imageBig9.addEventListener("click", makeBig9);
+make_imageBig10.addEventListener("click", makeBig10);
+make_imageBig11.addEventListener("click", makeBig11);
+make_imageBig12.addEventListener("click", makeBig12);
+make_imageBig13.addEventListener("click", makeBig13);
+make_imageBig14.addEventListener("click", makeBig14);
+make_imageBig15.addEventListener("click", makeBig15);
+make_imageBig16.addEventListener("click", makeBig16);
+make_imageBig17.addEventListener("click", makeBig17);
+make_imageBig18.addEventListener("click", makeBig18);
+make_imageBig19.addEventListener("click", makeBig19);
+make_imageBig20.addEventListener("click", makeBig20);
+make_imageBig21.addEventListener("click", makeBig21);
+make_imageBig22.addEventListener("click", makeBig22);
+
+
+
+
+
+
+
+
+
+
+const displayNav = document.querySelector(".nav-bottom");
+const navButton = document.getElementById("nav-toggle");
+
+
+navButton.addEventListener("click", function display() {
+
+   const makeVisible = displayNav.getAttribute("data-visible");
+
+   if (makeVisible === "false") {
+
+    displayNav.setAttribute("data-visible", true);
+
+    } else if (makeVisible === "true") {
+        displayNav.setAttribute("data-visible", false);
+    }
+    const rotateButton = navButton.getAttribute("data-visible");
+
+     if (rotateButton === "false") {
+        navButton.setAttribute("data-visible", true);
+     } else if (rotateButton === "true") {
+        navButton.setAttribute("data-visible", false);
+     }
+
+})
+
+
+/*******  PLAYER SECTION *******/
+
+
+// CONSTS
+
+const musicPlayer = document.querySelector(".player-section1");
+const musicPlayer2 = document.querySelector(".player-section2");
+const playBtn = document.querySelector("#playButton1");
+const playBtn2 = document.getElementById("playButton2");
+const changeTitle = document.querySelector(".current-song");
+const changeTitle2 = document.querySelector(".current-song2");
+const currentTimer1 = document.querySelector(".current-time1");
+const totalTime1 = document.querySelector(".total-time1");
+const currentTimer2 = document.querySelector(".current-time2");
+const totalTime2 = document.querySelector(".total-time2");
+const audio1 = document.querySelector("#audio-song1");
+const audio2 = document.querySelector("#audio-song2");
+const progressBar1 = document.querySelector(".progress-bar1");
+const progressBar2 = document.querySelector(".progress-bar2");
+const progressBarContainer2 = document.querySelector(".pb-container2");
+const progressBarContainer = document.querySelector(".pb-container");
+const timerContainer_1 = document.querySelector("#timer1");
+const timerContainer_2 = document.querySelector("#timer2");
+
+
+
+const selectSong1 = document.querySelector("#song-list1");
+const selectSong2 = document.querySelector("#song-list2");
+const selectSong3 = document.querySelector("#song-list3");
+const selectSong4 = document.querySelector("#song-list4");
+const selectSong5 = document.querySelector("#song-list5");
+
+
+const songList = document.querySelector(".song-name");
+
+const songs2 = ["Un Poco de Ti"];
+const songs = ["Me Recordare", "Mis Miedos Sin Ti", "Siempre Estas Tu", "Un Besito en la Boquita", "Un poco de Ti"];
+
+let songIndex = 4;
+
+let songIndex2 = 0;
+
+
+
+
+// FUCNTIONs
+
+
+function loadSongs(song) {
+
+   // changeTitle.innerHTML = song;
+   audio1.src = `audio/${song}.mp3`;
+}
+loadSongs(songs2[songIndex2]);
+
+
+
+function playMusic1() {
+   musicPlayer.classList.add("play");
+   playBtn.querySelector(".fa-solid").classList.remove("fa-play");
+   playBtn.querySelector(".fa-solid").classList.add("fa-pause");
+
+   audio1.play();
+}
+
+function pauseMusic1() {
+   musicPlayer.classList.remove("play")
+   playBtn.querySelector(".fa-solid").classList.remove("fa-pause");
+   playBtn.querySelector(".fa-solid").classList.add("fa-play");
+
+   audio1.pause();
+}
+
+function playMusic2() {
+   musicPlayer2.classList.add("play");
+   playBtn2.querySelector(".fa-solid").classList.remove("fa-play");
+   playBtn2.querySelector(".fa-solid").classList.add("fa-pause");
+
+   audio2.play();
+}
+
+function pauseMusic2() {
+   musicPlayer2.classList.remove("play")
+   playBtn2.querySelector(".fa-solid").classList.remove("fa-pause");
+   playBtn2.querySelector(".fa-solid").classList.add("fa-play");
+
+   audio2.pause();
+}
+
+
+function progressUpdate(e) {
+
+   const { duration, currentTime } = e.srcElement;
+   const progressBarUpdate = (currentTime / duration) * 100;
+
+   progressBar1.style.width = `${progressBarUpdate}%`;
+   
+   function timer() {
+
+      const minutes = Math.floor(audio1.duration);
+      const seconds = Math.floor(audio1.currentTime);
+      
+      if (seconds < 10) {
+         timerContainer_1.innerText = `00:0${seconds}/00:${minutes}`;
+      } else {
+         timerContainer_1.innerText = `00:${seconds}/00:${minutes}`;
+      }
+
+   }
+   timer()
+   
+}
+
+function progressUpdate2(e) {
+
+   const { duration, currentTime } = e.srcElement;
+   const progressBarUpdate2 = (currentTime / duration) * 100;
+
+   progressBar2.style.width = `${progressBarUpdate2}%`;
+   
+
+   function timer2() {
+
+      const minutes2 = Math.floor(audio2.duration);
+      const seconds2 = Math.floor(audio2.currentTime);
+      
+      if (seconds2 < 10) {
+         timerContainer_2.innerText = `00:0${seconds2}/00:${minutes2}`;
+      } else {
+         timerContainer_2.innerText = `00:${seconds2}/00:${minutes2}`;
+      }
+
+   }
+   timer2()
+  
+   
+}
+
+
+
+function setProgress(e) {
+
+   const width = this.clientWidth;
+   const clickX = e.offsetX;
+   const duration = audio1.duration;
+
+   audio1.currentTime = (clickX / width) * duration;
+
+}
+function setProgress2(e) {
+
+   const width_2 = this.clientWidth;
+   const clickX_2 = e.offsetX;
+   const duration_2 = audio2.duration;
+
+   audio2.currentTime = (clickX_2 / width_2) * duration_2;
+
+}
+
+
+
+
+
+// EVENT LISTENERS 
+
+playBtn.addEventListener("click", function musicPlaying() {
+
+   const playing = musicPlayer.classList.contains("play");
+
+      if (playing) {
+         pauseMusic1()
+      } else {
+         playMusic1()
+      }
+
+});
+playBtn2.addEventListener("click", function musicPlaying() {
+
+   const playing = musicPlayer2.classList.contains("play");
+
+      if (playing) {
+         pauseMusic2()
+      } else {
+         playMusic2()
+      }
+
+});
+
+
+selectSong1.addEventListener("click", function changeSong() {
+
+    changeTitle2.innerText = selectSong1.textContent;
+    audio2.src = `audio/${selectSong1.textContent}.mp3`;
+
+    if (selectSong1) {
+      songIndex 
+    } 
+
+    audio2.play();
+});
+selectSong2.addEventListener("click", function changeSong() {
+
+    changeTitle2.innerText = selectSong2.textContent;
+    audio2.src = `audio/${selectSong2.textContent}.mp3`;
+
+    if (selectSong2) {
+      songIndex++; 
+    } 
+
+    audio2.play();
+});
+selectSong3.addEventListener("click", function changeSong() {
+
+    changeTitle2.innerText = selectSong3.textContent;
+    audio2.src = `audio/${selectSong3.textContent}.mp3`;
+
+    if (selectSong3) {
+      songIndex = songIndex + 2;
+    } 
+
+    audio2.play();
+});
+selectSong4.addEventListener("click", function changeSong() {
+
+    changeTitle2.innerText = selectSong4.textContent;
+    audio2.src = `audio/${selectSong4.textContent}.mp3`;
+
+    if (selectSong4) {
+      songIndex = songIndex + 3;
+    } 
+
+    audio2.play();
+
+});
+selectSong5.addEventListener("click", function changeSong() {
+
+    changeTitle2.innerText = selectSong5.textContent;
+    audio2.src = `audio/${selectSong5.textContent}.mp3`;
+
+    if (selectSong5) {
+      songIndex = songIndex + 3;
+    } 
+
+    audio2.play();
+});
+
+audio1.addEventListener("timeupdate", progressUpdate);
+audio2.addEventListener("timeupdate", progressUpdate2);
+
+
+progressBarContainer.addEventListener("click", setProgress);
+progressBarContainer2.addEventListener("click", setProgress2);
+
+
+
+
+/*******************************************************  
+ * 
+ * Gallery Pop-up 
+ * 
+ * *****************************************************/
+
+
