7662d5fd851122a4b5f5c6b8f9b727a0d3dc4b55
Pablo Marzol <pablomarzol@yandex.com>
2022-09-29 00:40:01 +0800

    gallery bug fixed

---
+++ gallery.js
@@ -0,0 +1,393 @@
+const make_imageBig1 = window.document.querySelector(".photo_1");
+const make_imageBig2 = window.document.querySelector(".photo_2");
+const make_imageBig3 = window.document.querySelector(".photo_3");
+const make_imageBig4 = window.document.querySelector(".photo_4");
+const make_imageBig5 = window.document.querySelector(".photo_5");
+const make_imageBig6 = window.document.querySelector(".photo_6");
+const make_imageBig7 = window.document.querySelector(".photo_7");
+const make_imageBig8 = window.document.querySelector(".photo_8");
+const make_imageBig9 = window.document.querySelector(".photo_9");
+const make_imageBig10 = window.document.querySelector(".photo_10");
+const make_imageBig11 = window.document.querySelector(".photo_11");
+const make_imageBig12 = window.document.querySelector(".photo_12");
+const make_imageBig13 = window.document.querySelector(".photo_13");
+const make_imageBig14 = window.document.querySelector(".photo_14");
+const make_imageBig15 = window.document.querySelector(".photo_15");
+const make_imageBig16 = window.document.querySelector(".photo_16");
+const make_imageBig17 = window.document.querySelector(".photo_17");
+const make_imageBig18 = window.document.querySelector(".photo_18");
+const make_imageBig19 = window.document.querySelector(".photo_19");
+const make_imageBig20 = window.document.querySelector(".photo_20");
+const make_imageBig21 = window.document.querySelector(".photo_21");
+const make_imageBig22= window.document.querySelector(".photo_22");
+
+
+function makeBig1() {
+   
+   const bigPhoto1 = make_imageBig1.getAttribute("data-visible");
+
+   if (bigPhoto1 === "false") {
+
+      make_imageBig1.setAttribute("data-visible", true);
+
+   } else if (bigPhoto1 === "true") {
+
+      make_imageBig1.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig2() {
+   
+   const bigPhoto2 = make_imageBig2.getAttribute("data-visible");
+
+   if (bigPhoto2 === "false") {
+
+      make_imageBig2.setAttribute("data-visible", true);
+
+   } else if (bigPhoto2 === "true") {
+
+      make_imageBig2.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig3() {
+   
+   const bigPhoto3 = make_imageBig3.getAttribute("data-visible");
+
+   if (bigPhoto3 === "false") {
+
+      make_imageBig3.setAttribute("data-visible", true);
+
+   } else if (bigPhoto3 === "true") {
+
+      make_imageBig3.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig4() {
+   
+   const bigPhoto4 = make_imageBig4.getAttribute("data-visible");
+
+   if (bigPhoto4 === "false") {
+
+      make_imageBig4.setAttribute("data-visible", true);
+
+   } else if (bigPhoto4 === "true") {
+
+      make_imageBig4.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig5() {
+   
+   const bigPhoto5 = make_imageBig5.getAttribute("data-visible");
+
+   if (bigPhoto5 === "false") {
+
+      make_imageBig5.setAttribute("data-visible", true);
+
+   } else if (bigPhoto5 === "true") {
+
+      make_imageBig5.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig6() {
+   
+   const bigPhoto6 = make_imageBig6.getAttribute("data-visible");
+
+   if (bigPhoto6 === "false") {
+
+      make_imageBig6.setAttribute("data-visible", true);
+
+   } else if (bigPhoto6 === "true") {
+
+      make_imageBig6.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig7() {
+   
+   const bigPhoto7 = make_imageBig7.getAttribute("data-visible");
+
+   if (bigPhoto7 === "false") {
+
+      make_imageBig7.setAttribute("data-visible", true);
+
+   } else if (bigPhoto7 === "true") {
+
+      make_imageBig7.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig8() {
+   
+   const bigPhoto8 = make_imageBig8.getAttribute("data-visible");
+
+   if (bigPhoto8 === "false") {
+
+      make_imageBig8.setAttribute("data-visible", true);
+
+   } else if (bigPhoto8 === "true") {
+
+      make_imageBig8.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig9() {
+   
+   const bigPhoto9 = make_imageBig9.getAttribute("data-visible");
+
+   if (bigPhoto9 === "false") {
+
+      make_imageBig9.setAttribute("data-visible", true);
+
+   } else if (bigPhoto9 === "true") {
+
+      make_imageBig9.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig10() {
+   
+   const bigPhoto10 = make_imageBig10.getAttribute("data-visible");
+
+   if (bigPhoto10 === "false") {
+
+      make_imageBig10.setAttribute("data-visible", true);
+
+   } else if (bigPhoto10 === "true") {
+
+      make_imageBig10.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig11() {
+   
+   const bigPhoto11 = make_imageBig11.getAttribute("data-visible");
+
+   if (bigPhoto11 === "false") {
+
+      make_imageBig11.setAttribute("data-visible", true);
+
+   } else if (bigPhoto11 === "true") {
+
+      make_imageBig11.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig12() {
+   
+   const bigPhoto12 = make_imageBig12.getAttribute("data-visible");
+
+   if (bigPhoto12 === "false") {
+
+      make_imageBig12.setAttribute("data-visible", true);
+
+   } else if (bigPhoto12 === "true") {
+
+      make_imageBig12.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig13() {
+   
+   const bigPhoto13 = make_imageBig13.getAttribute("data-visible");
+
+   if (bigPhoto13 === "false") {
+
+      make_imageBig13.setAttribute("data-visible", true);
+
+   } else if (bigPhoto13 === "true") {
+
+      make_imageBig13.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig14() {
+   
+   const bigPhoto14 = make_imageBig14.getAttribute("data-visible");
+
+   if (bigPhoto14 === "false") {
+
+      make_imageBig14.setAttribute("data-visible", true);
+
+   } else if (bigPhoto14 === "true") {
+
+      make_imageBig14.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig15() {
+   
+   const bigPhoto15 = make_imageBig15.getAttribute("data-visible");
+
+   if (bigPhoto15 === "false") {
+
+      make_imageBig15.setAttribute("data-visible", true);
+
+   } else if (bigPhoto15 === "true") {
+
+      make_imageBig15.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig16() {
+   
+   const bigPhoto16 = make_imageBig16.getAttribute("data-visible");
+
+   if (bigPhoto16 === "false") {
+
+      make_imageBig16.setAttribute("data-visible", true);
+
+   } else if (bigPhoto16 === "true") {
+
+      make_imageBig16.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig17() {
+   
+   const bigPhoto17 = make_imageBig17.getAttribute("data-visible");
+
+   if (bigPhoto17 === "false") {
+
+      make_imageBig17.setAttribute("data-visible", true);
+
+   } else if (bigPhoto17 === "true") {
+
+      make_imageBig17.setAttribute("data-visible", false);
+   }
+
+}
+
+function makeBig18() {
+   
+   const bigPhoto18 = make_imageBig18.getAttribute("data-visible");
+
+   if (bigPhoto18 === "false") {
+
+      make_imageBig18.setAttribute("data-visible", true);
+
+   } else if (bigPhoto18 === "true") {
+
+      make_imageBig18.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig19() {
+   
+   const bigPhoto19 = make_imageBig19.getAttribute("data-visible");
+
+   if (bigPhoto19 === "false") {
+
+      make_imageBig19.setAttribute("data-visible", true);
+
+   } else if (bigPhoto19 === "true") {
+
+      make_imageBig19.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig20() {
+   
+   const bigPhoto20 = make_imageBig20.getAttribute("data-visible");
+
+   if (bigPhoto20 === "false") {
+
+      make_imageBig20.setAttribute("data-visible", true);
+
+   } else if (bigPhoto20 === "true") {
+
+      make_imageBig20.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig21() {
+   
+   const bigPhoto21 = make_imageBig21.getAttribute("data-visible");
+
+   if (bigPhoto21 === "false") {
+
+      make_imageBig21.setAttribute("data-visible", true);
+
+   } else if (bigPhoto21 === "true") {
+
+      make_imageBig21.setAttribute("data-visible", false);
+   }
+
+}
+
+
+function makeBig22() {
+   
+   const bigPhoto22 = make_imageBig22.getAttribute("data-visible");
+
+   if (bigPhoto22 === "false") {
+
+      make_imageBig22.setAttribute("data-visible", true);
+
+   } else if (bigPhoto22 === "true") {
+
+      make_imageBig22.setAttribute("data-visible", false);
+   }
+
+}
+
+make_imageBig1.addEventListener("click", makeBig1);
+make_imageBig2.addEventListener("click", makeBig2);
+make_imageBig3.addEventListener("click", makeBig3);
+make_imageBig4.addEventListener("click", makeBig4);
+make_imageBig5.addEventListener("click", makeBig5);
+make_imageBig6.addEventListener("click", makeBig6);
+make_imageBig7.addEventListener("click", makeBig7);
+make_imageBig8.addEventListener("click", makeBig8);
+make_imageBig9.addEventListener("click", makeBig9);
+make_imageBig10.addEventListener("click", makeBig10);
+make_imageBig11.addEventListener("click", makeBig11);
+make_imageBig12.addEventListener("click", makeBig12);
+make_imageBig13.addEventListener("click", makeBig13);
+make_imageBig14.addEventListener("click", makeBig14);
+make_imageBig15.addEventListener("click", makeBig15);
+make_imageBig16.addEventListener("click", makeBig16);
+make_imageBig17.addEventListener("click", makeBig17);
+make_imageBig18.addEventListener("click", makeBig18);
+make_imageBig19.addEventListener("click", makeBig19);
+make_imageBig20.addEventListener("click", makeBig20);
+make_imageBig21.addEventListener("click", makeBig21);
+make_imageBig22.addEventListener("click", makeBig22);
